cbc1861473544f54283873d49b73e110e828b5c4
Csehi Ágoston István <csehi.agoston1999@gmail.com>
2024-05-17 01:42:48 +0200

    sliding, timing

--- content.js
+++ content.js
@@ -1,47 +1,88 @@
+function injectCSS(fileName) {
+    const link = document.createElement('link');
+    link.href = chrome.runtime.getURL(fileName);
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+    document.head.appendChild(link);
+}
+
+injectCSS('styles.css');
+
+let prevButton = null;
+
+function applySlideEffectAndAutoClick(button) {
+    if (prevButton === button) return;
+    if (button.classList.contains('button-slide-effect')) return;
+    prevButton = button;
+    console.log('sliding start');
+    button.classList.add('button-slide-effect');
+
+    // Set a timer to click the button after the animation duration (2 seconds)
+    let timerId = setTimeout(() => {
+        button.click();
+        console.log('button clicked');
+    }, 5100);
+
+    function removeAnimationAndTimer() {
+        button.classList.remove('button-slide-effect');
+        clearTimeout(timerId);
+        document.removeEventListener('mousemove', removeAnimationAndTimer);
+        console.log('mouse moved during animation');
+    }
+
+    setTimeout(() => {
+        document.addEventListener('mousemove', removeAnimationAndTimer);
+    }, 500);
+
+}
+
+
 let autoSkipIntroEnabled = true;
 let autoSkipRecapEnabled = true;
 
 // Load initial state from storage
-chrome.storage.sync.get(['autoSkipIntroEnabled', 'autoSkipRecapEnabled'], function(result) {
-  autoSkipIntroEnabled = result.autoSkipIntroEnabled !== undefined ? result.autoSkipIntroEnabled : true;
-  autoSkipRecapEnabled = result.autoSkipRecapEnabled !== undefined ? result.autoSkipRecapEnabled : true;
+chrome.storage.local.get(['autoSkipIntroEnabled', 'autoSkipRecapEnabled'], function (result) {
+    console.log(`initial intro: ${result.autoSkipIntroEnabled} recap: ${result.autoSkipRecapEnabled}`)
+    autoSkipIntroEnabled = result.autoSkipIntroEnabled !== undefined ? result.autoSkipIntroEnabled : true;
+    autoSkipRecapEnabled = result.autoSkipRecapEnabled !== undefined ? result.autoSkipRecapEnabled : true;
 });
 
 // Listen for messages to enable/disable auto-skip
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.autoSkipIntroEnabled !== undefined) {
-    autoSkipIntroEnabled = request.autoSkipIntroEnabled;
-  }
-  if (request.autoSkipRecapEnabled !== undefined) {
-    autoSkipRecapEnabled = request.autoSkipRecapEnabled;
-  }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.autoSkipIntroEnabled !== undefined) {
+        autoSkipIntroEnabled = request.autoSkipIntroEnabled;
+        console.log(`Skip set to ${request.autoSkipIntroEnabled}`);
+    }
+    if (request.autoSkipRecapEnabled !== undefined) {
+        autoSkipRecapEnabled = request.autoSkipRecapEnabled;
+        console.log(`Recap set to ${request.autoSkipRecapEnabled}`);
+    }
 });
 
 function skipContent() {
-  if (autoSkipIntroEnabled) {
-    const skipIntroButton = document.querySelector('button[data-uia="player-skip-intro"]');
-    if (skipIntroButton) {
-      skipIntroButton.click();
-      console.log('Skipped Intro');
+    if (autoSkipIntroEnabled) {
+        const skipIntroButton = document.querySelector('[data-uia="player-skip-intro"]');
+        if (skipIntroButton) {
+            console.log('Skip');
+            applySlideEffectAndAutoClick(skipIntroButton);
+        }
     }
-  }
-
-  if (autoSkipRecapEnabled) {
-    const skipRecapButton = document.querySelector('button[data-uia="player-skip-recap"]');
-    if (skipRecapButton) {
-      skipRecapButton.click();
-      console.log('Skipped Recap');
+    if (autoSkipRecapEnabled) {
+        const skipRecapButton = document.querySelector('[data-uia="player-skip-recap"]');
+        if (skipRecapButton) {
+            console.log('Skip');
+            applySlideEffectAndAutoClick(skipRecapButton);
+        }
     }
-  }
 }
 
 // Use MutationObserver to monitor changes in the DOM and check for the "Skip Intro" and "Skip Recap" buttons
 const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    if (mutation.addedNodes.length) {
-      skipContent();
-    }
-  });
+    mutations.forEach((mutation) => {
+        if (mutation.addedNodes.length) {
+            skipContent();
+        }
+    });
 });
 
 observer.observe(document, { childList: true, subtree: true });
--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,22 @@
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true, url: "https://www.netflix.com/*"};
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+async function sendMessageToActiveTab(message) {
+  const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+  const response = await chrome.tabs.sendMessage(tab.id, message);
+  // TODO: Do something with the response.
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const introToggleSwitch = document.getElementById('intro-toggle-switch');
     const recapToggleSwitch = document.getElementById('recap-toggle-switch');
   
     // Load the current state from storage
-    chrome.storage.sync.get(['autoSkipIntroEnabled', 'autoSkipRecapEnabled'], function(result) {
+    chrome.storage.local.get(['autoSkipIntroEnabled', 'autoSkipRecapEnabled'], function(result) {
       introToggleSwitch.checked = result.autoSkipIntroEnabled || false;
       recapToggleSwitch.checked = result.autoSkipRecapEnabled || false;
     });
@@ -11,23 +24,28 @@ document.addEventListener('DOMContentLoaded', function() {
     // Add event listener for the intro toggle switch
     introToggleSwitch.addEventListener('change', function() {
       const isEnabled = introToggleSwitch.checked;
-      chrome.storage.sync.set({ autoSkipIntroEnabled: isEnabled });
-      
-      // Send message to content script to enable/disable skipping intro
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { autoSkipIntroEnabled: isEnabled });
-      });
+      chrome.storage.local.set({ autoSkipIntroEnabled: isEnabled });
+
+      (async () => {
+        const tab = await getCurrentTab();
+        if (tab !== undefined) {
+          const response = await chrome.tabs.sendMessage(tab.id, { autoSkipIntroEnabled: isEnabled });
+          console.log(response);
+        }
+      })();
     });
-  
-    // Add event listener for the recap toggle switch
+
     recapToggleSwitch.addEventListener('change', function() {
       const isEnabled = recapToggleSwitch.checked;
-      chrome.storage.sync.set({ autoSkipRecapEnabled: isEnabled });
-      
-      // Send message to content script to enable/disable skipping recap
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { autoSkipRecapEnabled: isEnabled });
-      });
+      chrome.storage.local.set({ autoSkipRecapEnabled: isEnabled });
+
+      (async () => {
+        const tab = await getCurrentTab();
+        if (tab !== undefined) {
+          const response = await chrome.tabs.sendMessage(tab.id, { autoSkipRecapEnabled: isEnabled });
+          console.log(response);
+        }
+      })();
     });
   });
 
