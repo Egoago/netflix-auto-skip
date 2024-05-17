2d242f62191ce84d2a0e537f7f37090ba446b752
Csehi Ágoston István <csehi.agoston1999@gmail.com>
2024-05-17 02:00:13 +0200

    refactor

---
+++ sliding/sliding.js
@@ -0,0 +1,37 @@
+function injectCSS(fileName) {
+    const link = document.createElement('link');
+    link.href = chrome.runtime.getURL(fileName);
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+    document.head.appendChild(link);
+}
+
+injectCSS('styles.css');
+
+let prevButton = null;
+
+function applySlideEffectAndAutoClick(button) {
+    if (prevButton === button) return;
+    if (button.classList.contains('button-slide-effect')) return;
+    prevButton = button;
+    console.log('sliding start');
+    button.classList.add('button-slide-effect');
+
+    // Set a timer to click the button after the animation duration (2 seconds)
+    let timerId = setTimeout(() => {
+        button.click();
+        console.log('button clicked');
+    }, 5100);
+
+    function removeAnimationAndTimer() {
+        button.classList.remove('button-slide-effect');
+        clearTimeout(timerId);
+        document.removeEventListener('mousemove', removeAnimationAndTimer);
+        console.log('mouse moved during animation');
+    }
+
+    setTimeout(() => {
+        document.addEventListener('mousemove', removeAnimationAndTimer);
+    }, 500);
+
+}
